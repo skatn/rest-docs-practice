a6d68577e52d284a41f15bc5da151f0a898eba20
skatn <87296704+skatn@users.noreply.github.com>
2023-11-09 17:16:40 +0900

    Initial commit

---
+++ src/main/java/com/example/restdocspractice/RestDocsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.restdocspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDocsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestDocsPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restdocspractice/RestDocsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restdocspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDocsPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
